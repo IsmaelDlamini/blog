10a7dc2702fd34059da2f304537d7627d5ffa504
Ismail <iii409475@gmail.com>
2024-11-20 22:55:02 +0200

    created routes for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -8,6 +13,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/create" element={<CreatePost />} />
         </Routes>
       </Router>
     </>
