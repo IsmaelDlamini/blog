e5f1de38e1b6a35aec8c3af79d2855ec73813699
Ismail <iii409475@gmail.com>
2025-04-02 02:11:52 +0200

    update axios version and refactor Home component to fetch posts from API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,60 +8,48 @@ import blog_image_extra_2 from "../assets/blog-image-extra2.jpg";
 import blog_image_extra_3 from "../assets/blog-image-extra3.jpg";
 import Footer from "../components/Footer";
 import BlogObject from "../components/BlogObject";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CallToAction from "../components/CallToAction";
 import HeroSection from "../components/HeroSection";
 import FeaturedPost from "../components/FeaturedPost";
+import axios, { isCancel, AxiosError } from "axios";
 
 const Home = () => {
-  // data for the extra posts
-  const [extraPosts, setExtraPosts] = useState([
-    {
-      PostType: "BLOG",
-      DatePosted: "20 January 2024",
-      PostLenght: "2-Mins",
-      PostTitle:
-        "Side Hustles that you can undertake as a University student to make more cash",
-      PostDescription:
-        "Discover profitable side hustles for university students to earn extra cash while managing studies.",
-      PostImage: blog_image_extra_1,
-    },
 
-    {
-      PostType: "BLOG",
-      DatePosted: "20 January 2024",
-      PostLenght: "2-Mins",
-      PostTitle: "Is AI going to replace software engineers?",
-      PostDescription:
-        "Explore if AI will replace software engineers or redefine their roles in the tech industry.",
-      PostImage: blog_image_extra_2,
-    },
+  const [posts, setPosts] = useState([]);
 
-    {
-      PostType: "BLOG",
-      DatePosted: "20 January 2024",
-      PostLenght: "2-Mins",
-      PostTitle:
-        "Top Coding Projects to Build Your Skills as a Software Engineer.",
-      PostDescription:
-        "Explore coding projects to enhance your skills and boost your software engineering portfolio.",
-      PostImage: blog_image_extra_3,
-    },
-  ]);
+  const api_url = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
-  const loadPosts = extraPosts.map((post, index) => {
-    return (
-      <BlogObject
-        PostType={post.PostType}
-        DateCreated={post.DatePosted}
-        PostLenght={post.PostLenght}
-        PostImage={post.PostImage}
-        PostDescription={post.PostDescription}
-        PostTitle={post.PostTitle}
-        key={index}
-      />
-    );
-  });
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await axios.get(
+          `${api_url}/api/posts`
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  const loadPosts = posts
+    .filter((post) => !post.featured)
+    .map((post) => {
+      return (
+        <BlogObject
+          PostType={post.PostType}
+          DateCreated={(post.createdAt) ? post.createdAt.split("T")[0] : ""}
+          PostLenght={post.PostLenght}
+          PostImage={post.PostImage}
+          PostDescription={post.PostDescription}
+          PostTitle={post.PostTitle}
+          key={post._id}
+        />
+      );
+    });
 
   return (
     <>
@@ -70,16 +58,27 @@ const Home = () => {
         <HeroSection />
 
         <div className=" w-[1000px] mx-auto">
-          <FeaturedPost
-            postType="BLOG"
-            image={
-              <img src={blog_image_featured} alt="" className="w-full h-full" />
-            }
-            datePosted="23 August 2024"
-            readTime="2-Mins"
-            title="How to Prepare yourself for the Workplace as a programmer"
-            description="Discover essential tips and strategies to prepare yourself for the workplace as a programmer. From building a solid technical foundation and mastering soft skills to creating a professional portfolio and acing job interviews, this guide will help you transition smoothly into your programming career."
-          />
+          {posts
+            .filter((post) => post.featured)
+            .map((post) => {
+              return (
+                <FeaturedPost
+                  key={post._id}
+                  postType="BLOG"
+                  image={
+                    <img
+                      src={post.PostImage}
+                      alt="featured post image"
+                      className="w-full h-full"
+                    />
+                  }
+                  datePosted={(post.createdAt) ? post.createdAt.split("T")[0] : ""}
+                  readTime={post.PostLenght}
+                  title={post.PostTitle}
+                  description={post.PostDescription}
+                />
+              );
+            })}
 
           <h2 className="mt-12 font-outfit text-neutral-500 font-light text-lg">
             More Reads
